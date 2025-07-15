d7b214e7753b87f38d76edc8f64f1388065f234b
Victor P <deerpg17x@gmail.com>
2025-07-14 22:05:30 -0400

    Plantilla

---
+++ flask-soft-ui-dashboard-master/apps/home/routes.py
@@ -0,0 +1,117 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+import wtforms
+from apps.home import blueprint
+from flask import render_template, request, redirect, url_for
+from jinja2 import TemplateNotFound
+from flask_login import login_required, current_user
+from apps import db
+from apps.authentication.models import Users
+from flask_wtf import FlaskForm
+
+@blueprint.route('/')
+@blueprint.route('/index')
+def index():
+    return render_template('pages/index.html', segment='dashboard', parent="dashboard")
+
+@blueprint.route('/billing')
+def billing():
+    return render_template('pages/billing.html', segment='billing')
+
+@blueprint.route('/rtl')
+def rtl():
+    return render_template('pages/rtl.html', segment='rtl')
+
+@blueprint.route('/tables')
+def tables():
+    return render_template('pages/tables.html', segment='tables')
+
+@blueprint.route('/virtual_reality')
+def virtual_reality():
+    return render_template('pages/virtual-reality.html', segment='virtual_reality')
+
+
+def getField(column): 
+    if isinstance(column.type, db.Text):
+        return wtforms.TextAreaField(column.name.title())
+    if isinstance(column.type, db.String):
+        return wtforms.StringField(column.name.title())
+    if isinstance(column.type, db.Boolean):
+        return wtforms.BooleanField(column.name.title())
+    if isinstance(column.type, db.Integer):
+        return wtforms.IntegerField(column.name.title())
+    if isinstance(column.type, db.Float):
+        return wtforms.DecimalField(column.name.title())
+    if isinstance(column.type, db.LargeBinary):
+        return wtforms.HiddenField(column.name.title())
+    return wtforms.StringField(column.name.title()) 
+
+
+@blueprint.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+
+    class ProfileForm(FlaskForm):
+        pass
+
+    readonly_fields = Users.readonly_fields
+    full_width_fields = {"bio"}
+
+    for column in Users.__table__.columns:
+        if column.name == "id":
+            continue
+
+        field_name = column.name
+        if field_name in full_width_fields:
+            continue
+
+        field = getField(column)
+        setattr(ProfileForm, field_name, field)
+
+    for field_name in full_width_fields:
+        if field_name in Users.__table__.columns:
+            column = Users.__table__.columns[field_name]
+            field = getField(column)
+            setattr(ProfileForm, field_name, field)
+
+    form = ProfileForm(obj=current_user)
+
+    if form.validate_on_submit():
+        readonly_fields.append("password")
+        excluded_fields = readonly_fields
+        for field_name, field_value in form.data.items():
+            if field_name not in excluded_fields:
+                setattr(current_user, field_name, field_value)
+
+        db.session.commit()
+        return redirect(url_for('home_blueprint.profile'))
+    
+    context = {
+        'segment': 'profile',
+        'form': form,
+        'readonly_fields': readonly_fields,
+        'full_width_fields': full_width_fields,
+    }
+    return render_template('pages/profile.html', **context)
+
+
+# Helper - Extract current page name from request
+@blueprint.app_template_filter('replace_value')
+def replace_value(value, args):
+  return value.replace(args, " ").title()
+
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
