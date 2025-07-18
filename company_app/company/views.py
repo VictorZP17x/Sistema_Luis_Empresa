3047306dc807d280c3b1a89be0d6db53e80d1e5d
danielavictor <danielavictornotion@gmail.com>
2025-07-18 13:11:36 -0400

    Pantalla de empresas con la tabla

--- company_app/company/views.py
+++ company_app/company/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
+from . models import Company
 
 # @login_required
 def company(request):
+    company = Company.objects.all()
     return render(request, 'company.html', {
+        'company': company,
     })
