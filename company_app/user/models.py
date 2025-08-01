b2b1a777f9387b7a0a4793c1b547bf45f0b62022
danielavictor <danielavictornotion@gmail.com>
2025-08-01 12:30:12 -0400

    creada relacion para que en auth user se registren trabajadores, rol 3, y en que empresa trabajan

--- company_app/user/models.py
+++ company_app/user/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from company.models import Company
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         (0, 'Administrador'),
         (1, 'Empleado'),
         (2, 'Cliente'),
+        (3, 'Trabajador'), #Se filtran por la empresa específica donde trabajan
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.IntegerField(choices=ROLE_CHOICES, default=0, null=False)
     phone = models.CharField(max_length=20, blank=True, null=True)
+    company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
