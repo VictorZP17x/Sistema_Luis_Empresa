d519a9a364390e6d901caae575933ed60a6f163f
danielavictor <danielavictornotion@gmail.com>
2025-08-05 17:17:35 -0400

    script en su js

--- company_app/static/assets/js/dashboard_js/dashboard.js
+++ company_app/static/assets/js/dashboard_js/dashboard.js
@@ -51,3 +51,80 @@ $(document).ready(function () {
   
 });
 
+$(document).ready(function () {
+  var workersDetail = window.workersDetail;
+  var empresaModalId = null;
+
+  $('.btn-ver-trabajadores').on('click', function () {
+    var companyId = $(this).data('company');
+    var serviceId = $(this).data('service');
+    empresaModalId = "#empresaModal" + companyId;
+    var trabajadores = (workersDetail[companyId] && workersDetail[companyId][serviceId]) ? workersDetail[companyId][serviceId] : [];
+
+    // Destruir DataTable anterior si existe
+    if ($.fn.DataTable.isDataTable('#tabla-trabajadores-modal')) {
+      $('#tabla-trabajadores-modal').DataTable().clear().destroy();
+    }
+
+    var $tbody = $('#tabla-trabajadores-modal tbody');
+    $tbody.empty();
+
+    if (!trabajadores || trabajadores.length === 0) {
+      $tbody.append(
+        `<tr>
+          <td class="text-center" colspan="4">No hay trabajadores asociados a este servicio.</td>
+          <td style="display:none"></td>
+          <td style="display:none"></td>
+          <td style="display:none"></td>
+        </tr>`
+      );
+    } else {
+      trabajadores.forEach(function (w) {
+        $tbody.append(
+          `<tr>
+            <td>
+              <img src="${w.photo ? w.photo : '/static/assets/img/default-user.png'}"
+                   class="rounded-circle" style="width:40px;height:40px;object-fit:cover;">
+            </td>
+            <td><strong>${w.name}</strong></td>
+            <td>${w.email}</td>
+            <td>${w.phone}</td>
+          </tr>`
+        );
+      });
+    }
+
+    // Inicializar DataTable con búsqueda y paginación de 6
+    $('#tabla-trabajadores-modal').DataTable({
+      paging: true,
+      pageLength: 6,
+      lengthChange: false,
+      searching: true,
+      ordering: false,
+      info: false,
+      language: {
+        search: "Buscar trabajador:",
+        paginate: {
+          previous: "Anterior",
+          next: "Siguiente"
+        },
+        zeroRecords: "No hay trabajadores asociados a este servicio."
+      }
+    });
+
+    // Sincronizar el input con el DataTable search
+    $('#buscador-trabajadores-modal').val('');
+    $('#buscador-trabajadores-modal').off('input').on('input', function () {
+      $('#tabla-trabajadores-modal').DataTable().search($(this).val()).draw();
+    });
+  });
+
+  $('#volver-ver-mas').on('click', function () {
+    $('#trabajadoresModal').modal('hide');
+    if (empresaModalId) {
+      setTimeout(function () {
+        $(empresaModalId).modal('show');
+      }, 500);
+    }
+  });
+});
